4736b8725ae4bdf524fe951e7031d73ae3b26c7e
Ryan Raub <ryan.raub@gmail.com>
2020-02-28 09:15:28 -0700

    adding tags to everything and enabling volume

--- variables.tf
+++ variables.tf
@@ -66,21 +66,36 @@ variable "log_retention_in_days" {
   type    = string
   default = 365
 }
+
 variable "access_log_bucket" {
   type        = string
   default     = "bucketname"
   description = "optional bucket name to use for access logs"
 }
+
 variable "access_log_bucket_prefix" {
   type        = string
   default     = ""
   description = "optional prefix to use for access logs"
 }
+
 variable "access_log_interval" {
   type    = string
   default = 60
 }
+
 variable "access_logs_enabled" {
   type    = bool
   default = false
+}
+
+variable "tags" {
+  type        = map(string)
+  default     = {}
+  description = "Optional additional Tags to add onto resources this module creates"
+}
+
+variable "encrypted_ebs_instance_volume" {
+  type    = bool
+  default = true
 }
